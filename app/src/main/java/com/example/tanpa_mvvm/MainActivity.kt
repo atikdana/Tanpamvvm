6b58ffc3b73e483801e8a10583dc4697e5fce0af
atikdana <atikprik@gmail.com>
2022-11-02 15:59:50 +0700

    tugas atik

---
+++ app/src/main/java/com/example/tanpa_mvvm/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.tanpa_mvvm
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.example.tanpa_mvvm.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private var counter = 0
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    private fun increaseCounter() {
+        counter += 1
+        binding.tvCounter.text= "$counter"
+    }
+    private fun decreaseCounter() {
+        counter -= 1
+        binding.tvCounter.text= "$counter"
+    }
+    override fun onClick(p0: View?) {
+        when (p0) {
+            binding.btnPlus -> increaseCounter()
+            binding.btnMinus -> decreaseCounter()
+        }
+    }
+}
+
